bcc645eeb8053056d05286020ece7041b9da9800
Mansi Wakharkar <mansiswakharkar@gmail.com>
2021-07-14 22:12:04 +0300

    Valid Parenthesis

--- LeetCodeAlgorithms/src/strings/easy/ValidParenthesis.java
+++ LeetCodeAlgorithms/src/strings/easy/ValidParenthesis.java
@@ -1,10 +1,53 @@
 package strings.easy;
 
+import java.util.Stack;
+
 public class ValidParenthesis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ValidParenthesis obj = new ValidParenthesis();
+		System.out.println(obj.isValid("()"));
+		System.out.println(obj.isValid("([])"));
+		System.out.println(obj.isValid("([{}])"));
+		System.out.println(obj.isValid("([)"));
+		System.out.println(obj.isValid("([]){}"));
+		System.out.println(obj.isValid("([])}"));
+		System.out.println(obj.isValid("(("));
+	}
 
+	public boolean isValid(String s) {
+
+		if(s==null || s.length()<2) {
+			return false;
+		}
+		
+		Stack<Character> stack = new Stack<>();
+		int n = s.length();
+		for(int i=0; i<n; i++) {
+			Character ch = s.charAt(i);
+			if((ch==']' || ch==')' || ch=='}') && stack.isEmpty()) {
+				return false;
+			}
+			if(ch=='[' || ch=='(' || ch=='{') {
+				stack.push(ch);
+			}else {
+				if(ch==']') {
+					if(stack.pop()!='[') {
+						return false;
+					}
+				}else if(ch=='}') {
+					if(stack.pop()!='{') {
+						return false;
+					}
+				}else if(ch==')') {
+					if(stack.pop()!='(') {
+						return false;
+					}
+				}
+			}
+		}
+		
+		return stack.isEmpty();
 	}
 
 }
